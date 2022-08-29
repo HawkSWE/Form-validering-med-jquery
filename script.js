9e0cfb162314c939ca34879488f5a5255134b5e3
HawkSWE <83948065+HawkSWE@users.noreply.github.com>
2022-08-29 12:40:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
-
     $('form').submit(function(){
         var name = $('input[id=name]').val();
-        alert(name);
-    });
+        var email = $('input[id=email]').val();
+        var password = $('input[id=password]').val();
+        var conpass = $('input[id=confirm_password]').val();
+        var terms = $('input[id=terms]:checked');
 
+        alert(terms);
+    });
 });
